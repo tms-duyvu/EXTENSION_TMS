ebdb06ea55efef817cea103b3459ccac7f145a75
tms-duyvu <tt.vu.nguyen@tomosia.com>
2021-04-27 22:30:16 +0700

    hehe

--- background.js
+++ background.js
@@ -4,102 +4,119 @@ import { employee } from './popup/popup.js'
 var arr_manager = [];
 var name_manager = {};
 chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-  let init = {
-    method: 'GET',
-    async: true,
-    headers: {
-      Authorization: 'Bearer ' + token,
-      'Content-Type': 'application/json'
-    },
-    'contentType': 'json'
-  };
-  fetch(
-      'https://www.googleapis.com/gmail/v1/users/me/profile?',
-      init)
-  .then((response) => response.json())
-  .then(function(data) {
-    console.log(data);
-    console.log(data.emailAddress);
-    global = data.emailAddress;
-  });
+    let init = {
+        method: 'GET',
+        async: true,
+        headers: {
+            Authorization: 'Bearer ' + token,
+            'Content-Type': 'application/json'
+        },
+        'contentType': 'json'
+    };
+    fetch(
+            'https://www.googleapis.com/gmail/v1/users/me/profile?',
+            init)
+        .then((response) => response.json())
+        .then(function(data) {
+            console.log(data);
+            console.log(data.emailAddress);
+            global = data.emailAddress;
+        });
+    // Promise.resolve(handleClientLoad('get_data_to_sheet'))
+    //     .then(addOptionElementToHTML).catch(err => {
+    //         throw err
+    //     })
+
+    // addOptionElementToHTML();
+    handleClientLoad('get_data_to_sheet');
 });
 
-function handleClientLoad() {
-  gapi.load('client:auth2', initClienForReadAndWrite(appendToSheets));
-    console.log("succsessully");
+function handleClientLoad(option) {
+    switch (option) {
+        case 'write_to_sheet':
+            gapi.load('client:auth2', initClienForReadAndWrite(appendToSheets));
+            break;
+        default:
+            gapi.load('client:auth2', initClienForReadAndWrite(getDataFromSheet));
+    }
 }
 
 
-function initClienForReadAndWrite(funtion1) {
-
-  var CLIENT_ID = '947005021816-1pifp31rnhn1hsv5amvq7f4as61i54sa.apps.googleusercontent.com';
-  var API_KEY = 'AIzaSyCnOUhyWNSAASHKevvFltlLlZwnx7O31Jo';
-  var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: DISCOVERY_DOCS,
-  }).then(function () {
-    console.log(gapi.client)
-    chrome.identity.getAuthToken({ interactive: true }, function (token) {
-      gapi.auth.setToken({
-        'access_token': token
-      });
-      funtion1();
+function initClienForReadAndWrite(callback) {
+    var CLIENT_ID = '947005021816-8dpsbm6icimgo5qujl3ihh92q8jbn11n.apps.googleusercontent.com';
+    var API_KEY = 'AIzaSyCnOUhyWNSAASHKevvFltlLlZwnx7O31Jo';
+    var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+    gapi.client.init({
+        apiKey: API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+    }).then(function() {
+        console.log(gapi.client)
+        chrome.identity.getAuthToken({ interactive: true }, function(token) {
+            gapi.auth.setToken({
+                'access_token': token
+            });
+            callback();
+        });
+    }, function(error) {
+        appendPre(JSON.stringify(error, null, 2));
     });
-  }, function (error) {
-    appendPre(JSON.stringify(error, null, 2));
-  });
 }
 
 function appendToSheets() {
-  const body = {
-    values: [
-      [
-        employee.name,
-        global,
-        employee.day_off,
-        employee.session,
-        employee.manager,
-        employee.reason
-      ]
-    ]
-  };
-  gapi.client.sheets.spreadsheets.values.append({
-    spreadsheetId: spreadsheetId,
-    range: "members",
-    valueInputOption: 'USER_ENTERED',
-    resource: body
-  }).then((response) => {
-    console.log("a1")
-    console.log(`${response.result.updates.updatedCells} cells appended.`)
-  });
+    const body = {
+        values: [
+            [
+                employee.name,
+                global,
+                employee.day_off,
+                employee.session,
+                employee.manager,
+                employee.reason
+            ]
+        ]
+    };
+    gapi.client.sheets.spreadsheets.values.append({
+        spreadsheetId: spreadsheetId,
+        range: "members",
+        valueInputOption: 'USER_ENTERED',
+        resource: body
+    }).then((response) => {
+        console.log("a1")
+        console.log(`${response.result.updates.updatedCells} cells appended.`)
+    });
 }
 
 function getDataFromSheet() {
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: "managers"
+    }).then(function(response) {
+        console.log(response.result.values)
+        console.log(typeof(response.result.values))
+        for (var i = 0; i < response.result.values.length; i++) {
+            arr_manager.push(response.result.values[i][0]);
+            arr_manager.push[name_manager];
+        }
+        console.log(`Got ${response.result.values.length} rows back`)
+            // for (let i = 0; i < arr_manager.length; i++) {
+            //     console.log(arr_manager[i])
+            // }
+        addOptionElementToHTML();
+    });
 
-  console.log("user : " + gapi.client.value)
-  gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: spreadsheetId,
-    range: "managers"
-  }).then(function (response) {
+}
 
-    console.log(response.result.values)
-    console.log(typeof (response.result.values))
- 
-    for (var i = 0; i < response.result.values.length; i++){
-      
-      arr_manager.push(response.result.values[i][0]);
-      arr_manager.push[name_manager];
+function addOptionElementToHTML() {
+    console.log("html")
+    for (let i = 0; i < arr_manager.length; i++) {
+        console.log(arr_manager[i])
+        let option_tag = document.createElement("OPTION");
+        let value = document.createTextNode(arr_manager[i]);
+        option_tag.appendChild(value);
+        document.getElementById("maneger").appendChild(option_tag);
     }
-    console.log(`Got ${response.result.values.length} rows back`)
-  });
+
 }
 
-function showAlert() {
-  gapi.load('client:auth2', initClienForReadAndWrite(getDataFromSheet));
-  for (let i = 0; i < arr_manager.length; i++) {
-    console.log(arr_manager[i]);
-  }
-}
-export { handleClientLoad, global, showAlert }
+export { handleClientLoad, global }
--- popup/popup.js
+++ popup/popup.js
@@ -1,51 +1,48 @@
-import { handleClientLoad, global, showAlert } from '../background.js'
+import { handleClientLoad, global } from '../background.js'
 export let employee = {};
 let my_form = document.getElementById("my_form");
 let raido = document.getElementsByName("session");
 let select_option = document.getElementById("maneger").value;
 
 my_form.addEventListener('submit', (event) => {
-  
-  console.log("hum")
-  employee = {
-    name: my_form[0].value,
-    day_off: my_form[1].value,
-    session: my_form[5].value,
-    reason: my_form[6].value,
-    maneger: select_option
-  }
-  // alert("name : " + employee.name + "\n" + "Day Off : " + employee.day_off + "\n" +
-  //   "Session : " + employee.session + "\n" + "reason : " + employee.reason + "\n" +
-  //   "maneger :" + employee.maneger + "\n" + global + "");
-  // handleClientLoad();
-  console.log("vo dc ko")
 
-  // if (confirm("Successfully" + "\n" + "Do you want exit")) {
-  //   window.close();
-  // };
-  debugger
-  showAlert();
-  event.preventDefault();
- 
+    console.log("hum")
+    employee = {
+            name: my_form[0].value,
+            day_off: my_form[1].value,
+            session: my_form[5].value,
+            reason: my_form[6].value,
+            maneger: select_option
+        }
+        // alert("name : " + employee.name + "\n" + "Day Off : " + employee.day_off + "\n" +
+        //   "Session : " + employee.session + "\n" + "reason : " + employee.reason + "\n" +
+        //   "maneger :" + employee.maneger + "\n" + global + "");
+        // handleClientLoad();
+    console.log("vo dc ko")
+        // if (confirm("Successfully" + "\n" + "Do you want exit")) {
+        //   window.close();
+        // }
+    handleClientLoad('write_to_sheet');
+    event.preventDefault();
+
 });
 
 function getRadioValue() {
-  let radio_checked;
-  for (let i = 0; i < raido.length; i++) {
-    if (raido[i].checked) {
-      switch (i) {
-        case 0:
-          radio_checked = "morning"
-          break;
-        case 1:
-          radio_checked = "afternoon"
-          break;
-        default:
-          radio_checked = "full day"
+    let radio_checked;
+    for (let i = 0; i < raido.length; i++) {
+        if (raido[i].checked) {
+            switch (i) {
+                case 0:
+                    radio_checked = "morning"
+                    break;
+                case 1:
+                    radio_checked = "afternoon"
+                    break;
+                default:
+                    radio_checked = "full day"
+            }
+            break;
+        }
     }
-    break;
-  }
-}
-  return radio_checked;
+    return radio_checked;
 };
-
