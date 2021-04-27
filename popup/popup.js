3fd03eb736d35fefc4dede63eb06f68051d0f86b
tms-duyvu <tt.vu.nguyen@tomosia.com>
2021-04-27 17:17:20 +0700

    hello

--- popup/popup.js
+++ popup/popup.js
@@ -1,51 +1,51 @@
-export { employee }
-import { handleClientLoad, global } from '../background.js'
-let employee = {};
+import { handleClientLoad, global, showAlert } from '../background.js'
+export let employee = {};
 let my_form = document.getElementById("my_form");
 let raido = document.getElementsByName("session");
 let select_option = document.getElementById("maneger").value;
 
-let spreadsheetId = '1KJABE6ZF-0N02lYt2NgD7YRJwer6QB7tNvQZp21Ys6Y';
-let range = 'members';
-let valueInputOption = 'RAW';
-
 my_form.addEventListener('submit', (event) => {
-    employee = {
-        name: my_form[0].value,
-        day_off: my_form[1].value,
-        session: my_form[5].value,
-        reason: my_form[6].value,
-        maneger: select_option
-    }
-    alert("name : " + employee.name + "\n" + "Day Off : " + employee.day_off + "\n" +
-        "Session : " + employee.session + "\n" + "reason : " + employee.reason + "\n" +
-        "maneger :" + employee.maneger + "\n" + global + "");
-    // if (confirm("Successfully" + "\n" + "Do you want exit")) {
-    //     window.close();
-    // };
-    chrome.runtime.sendMessage(employee, function(response) {
-        console.log('response', response);
-    });
+  
+  console.log("hum")
+  employee = {
+    name: my_form[0].value,
+    day_off: my_form[1].value,
+    session: my_form[5].value,
+    reason: my_form[6].value,
+    maneger: select_option
+  }
+  // alert("name : " + employee.name + "\n" + "Day Off : " + employee.day_off + "\n" +
+  //   "Session : " + employee.session + "\n" + "reason : " + employee.reason + "\n" +
+  //   "maneger :" + employee.maneger + "\n" + global + "");
+  // handleClientLoad();
+  console.log("vo dc ko")
 
+  // if (confirm("Successfully" + "\n" + "Do you want exit")) {
+  //   window.close();
+  // };
+  debugger
+  showAlert();
+  event.preventDefault();
+ 
 });
 
-
 function getRadioValue() {
-    let radio_checked;
-    for (let i = 0; i < raido.length; i++) {
-        if (raido[i].checked) {
-            switch (i) {
-                case 0:
-                    radio_checked = "morning"
-                    break;
-                case 1:
-                    radio_checked = "afternoon"
-                    break;
-                default:
-                    radio_checked = "full day"
-            }
-            break;
-        }
+  let radio_checked;
+  for (let i = 0; i < raido.length; i++) {
+    if (raido[i].checked) {
+      switch (i) {
+        case 0:
+          radio_checked = "morning"
+          break;
+        case 1:
+          radio_checked = "afternoon"
+          break;
+        default:
+          radio_checked = "full day"
     }
-    return radio_checked;
+    break;
+  }
+}
+  return radio_checked;
 };
+
